56112123c7e0cb5df5bf8d9db7bf3a708e2812c9
James Higgs <james.y.higgs@gmail.com>
2019-08-24 21:10:44 -0700

    accordion commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
-import Tabs from './state/Tabs.js';
+import Accordion from './state-drills/Accordion';
 /*
+import Tabs from './state/Tabs.js';
 import Messages from './Messages';
 import TheDate from './state/TheDate.js';
 import Counter from './state/Counter.js';
@@ -10,21 +11,26 @@ import Bomb from './state-drills/Bomb.js';
 import RouletteGun from './state-drills/RouletteGun';
 */
 
-
-const tabsProp = [
-  { name: 'First tab',
-  content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
-  { name: 'Second tab',
-  content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
-  { name: 'Third tab',
-  content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
-  ]
+const sectionsProp = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+];
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Tabs tabs={tabsProp} />
+        <Accordion sections={sectionsProp} />
       </div>
     );
   }
